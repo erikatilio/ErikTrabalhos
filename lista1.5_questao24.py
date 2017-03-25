cf1befe1f18c2a8de6126565b52dd3059b700967
erikatilio <erimom3000@gmail.com>
2017-03-25 12:03:16 -0400

    grupo 02

---
+++ lista1.5_questao24.py
@@ -0,0 +1,9 @@
+
+money = float(input("Com quanto dinheiro você vai viajar? R$: "))
+dollar = (money/1.8)
+german_mark = (money/2)
+pound_sterling = (money/1.57)
+
+print("Você tera em dolares: U$", round(dollar, 2))
+print("Você tera em marco alemão: DM", round(german_mark, 2))
+print("Você tera em libra esterlina: £", round(pound_sterling, 2))
