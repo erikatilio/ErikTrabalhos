ec4fac9c485993074a775572067e3b320a27f0a2
erikatilio <erimom3000@gmail.com>
2017-03-27 11:08:57 -0400

    teste do teste

---
+++ questao03.py
@@ -0,0 +1,7 @@
+#3.Faça um Programa que peça dois números e imprima a soma.
+#
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+result = n1+n2
+
+print("A soma dos dois números é: ", result)
